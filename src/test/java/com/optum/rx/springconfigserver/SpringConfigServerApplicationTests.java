a62a5d9a823ede558894542c6272d37297a614a4
Gujjula Jayaram <71973639+greddy16@users.noreply.github.com>
2020-10-06 19:14:30 +0530

    spring config server implementations

---
+++ src/test/java/com/optum/rx/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.optum.rx.springconfigserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
